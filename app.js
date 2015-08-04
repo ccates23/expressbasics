e101c97c551f059d5c2f4a45ffbb95327e03b10c
chadcates <cates.chad@gmail.com>
2015-08-04 10:39:16 -0500

    made loggly files in app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+var fs = require('fs');
 var express = require('express');
-
+var morgan = require('morgan');
+var loggly = require('loggly');
 var routes = require('./routes/index');
 var pizza = require('./routes/pizza');
 
@@ -15,6 +17,30 @@ app.use(function (req, res, next) {
   console.log('Request at ' + new Date().toISOString());
   next();
 });
+var logStream = fs.createWriteStream('access.log', {flags: 'a'});
+app.use(morgan('combined', {stream: logStream}));
+app.use(morgan('dev'));
+
+ var client = loggly.createClient({
+    token: "fe19754b-ea8c-44bc-a1b3-f26896e81cba",
+    subdomain: "chadcates",
+    tags: ["NodeJS"],
+    json:true
+});
+
+
+app.use(function (req, res, next) {
+  client.log({
+    ip: req.ip,
+    date: new Date(),
+    url: req.url,
+    status: req.statusCode,
+    method: req.method
+  });
+  next();
+});
+
+
 app.use(express.static('public'));
 
 app.use('/', routes);
